fca14e8986694c0bed48bc14ab6d457d68929cb8
bluVshal <blu.vishal@gmail.com>
2024-09-05 19:42:29 +0400

    Added other components and MUI package

---
+++ src/Components/Assemble/Assemble.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Screen from '../Screen/Screen';
+
+const Assemble = () => {
+    return(
+        <div>
+            <p>Assembled calculator will be here</p>
+            <Screen />
+        </div>
+    );
+};
+
+export default Assemble;
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Assemble from '../Components/Assemble/Assemble';
 
 const Calculator = () => {
     return(
         <div>
             <p>Sample Calculator</p>
+            <Assemble />
         </div>
     );
 }
