68303bac49f612792fb5bc35a057e694ebfc24a0
bluVshal <blu.vishal@gmail.com>
2024-09-06 11:32:09 +0400

    Modification done on Buttons and Memory items

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -7,13 +7,19 @@ const Buttons = () => {
     const populateNumberButtons = () =>{
         const arrayNum = [0,1,2,3,4,5,6,7,8,9];
 
-        return arrayNum.map((numbr) => <Button key={numbr} sx={{color: '#EE4421'}}>{numbr}</Button>);
+        return arrayNum.map((numbr) => <Button key={numbr} sx={{
+            color: '#EE4421',
+            fontSize: '1.5rem'
+            }}>{numbr}</Button>);
     };
 
     const populateoperationButtons = () => {
-        const arrayOp = ['.','+','-','×','÷',];
+        const arrayOp = ['.','+','-','×','÷','±','%','00'];
 
-        return arrayOp.map((op) => <Button sx={{color: '#ECF018'}}>{op}</Button>);
+        return arrayOp.map((op) => <Button key={op} sx={{
+            color: '#ECF018',
+            fontSize: '1.75rem'
+            }}>{op}</Button>);
     };
 
     return(
--- src/Components/Elements/Memory/Memory.js
+++ src/Components/Elements/Memory/Memory.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const Memory = () => {
+
+    const setBtnValue = (mem) => {
+        console.log("Inside");
+    };
+
+    const populateMemButtons = () =>{
+        const arrayMem = ['MR','M+','M-','CLS','CE'];
+
+        return arrayMem.map((mem) => <Button key={mem} onClick={setBtnValue(mem)} sx={{
+            color: '#00A86B',
+            fontSize: '1.5rem'
+            }}>{mem}</Button>);
+    };
+
     return (
         <div>
-            <p> Memory Element</p>
+            {populateMemButtons()}
         </div>
     );
 
